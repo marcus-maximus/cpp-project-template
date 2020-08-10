ed873857fdcff62ab8db5edf98a88598e3197f35
Marcus Schaber <marcus@hypercat.de>
2020-08-10 21:28:58 +0200

    Fix syntax calling cmake. Add vcpkg to gitignore.

--- build.ps1
+++ build.ps1
@@ -1,3 +1,3 @@
 New-Item build -ItemType Directory -ErrorAction Continue
-cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake -S . -B build -D CMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake
 cmake --build build
