f00b24c5d2f30960db0cd1addd3d1f953a9d7f0b
Marcus Schaber <marcus@hypercat.de>
2020-08-17 17:44:30 +0200

    Add TDD style test case

--- library/test/library_test.cpp
+++ library/test/library_test.cpp
@@ -7,3 +7,22 @@ TEST_CASE( "One plus one", "[add]" )
 {
 	REQUIRE(add(1, 1) == 2);
 }
+
+SCENARIO("Properties of add")
+{
+	GIVEN("Two positive values")
+	{
+		int a = 2;
+		int b = 3;
+
+		WHEN("they are added")
+		{
+			int sum = add(a, b);
+
+			THEN("the sum is positive")
+			{
+				REQUIRE(sum >= 0);
+			}
+		}
+	}
+}
