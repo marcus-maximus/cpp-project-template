c7dff3607f20631e5c3dd871ad2783ad34d4bb84
Marcus Schaber <marcus@hypercat.de>
2020-08-19 19:46:24 +0200

    Fix c++ syntax of property_test

--- library/test/property_test.cpp
+++ library/test/property_test.cpp
@@ -6,13 +6,13 @@ int main()
 {
     rc::check("add is commutative",
         [](const int a, const int b) {
-            RC_ASSERT(add(a, b) == add(b, a))
-        })
+            RC_ASSERT(add(a, b) == add(b, a));
+        });
 
     rc::check("add is associative",
         [](const int a, const int b, const int c) {
-            RC_ASSERT(add(a, add(b, c)) == add(add(a, b), c))
-        })
+            RC_ASSERT(add(a, add(b, c)) == add(add(a, b), c));
+        });
 
     return 0;
 }
