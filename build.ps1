1b96b88ba1674ff2612e7218ef818658489e5128
Marcus Schaber <marcus@hypercat.de>
2020-11-22 14:53:24 +0100

    Make build script parameterized

--- build.ps1
+++ build.ps1
@@ -1,3 +1,14 @@
-New-Item build -ItemType Directory -ErrorAction Continue
-cmake -S . -B build -D CMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake
-cmake --build build
+param(
+    [String]
+    $SourceDirectory = ".",
+
+    [String]
+    $BuildDirectory = "build",
+
+    [String]
+    $ToolChainFile = "vcpkg/scripts/buildsystems/vcpkg.cmake"
+)
+
+New-Item $BuildDirectory -ItemType Directory -ErrorAction Continue
+cmake -S $SourceDirectory -B $BuildDirectory -D CMAKE_TOOLCHAIN_FILE=$ToolChainFile
+cmake --build $BuildDirectory
