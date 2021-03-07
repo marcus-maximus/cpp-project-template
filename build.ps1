1e56470d185cb18cf7f4b9f21972fb893e757d47
Marcus Schaber <marcus@hypercat.de>
2021-03-07 22:39:34 +0100

    Add build type (Release/Debug) parameter.

--- build.ps1
+++ build.ps1
@@ -6,9 +6,22 @@ param(
     $BuildDirectory = "build",
 
     [String]
+    [ValidateSet("Release", "Debug")]
+    $BuildType = "Release",
+
+    [String]
     $ToolChainFile = "vcpkg/scripts/buildsystems/vcpkg.cmake"
 )
 
+
+if($isWindows) {
+	$AdditionalConfigureArguments = "-G", "Visual Studio 16 2019",  "-A", "x64"
+    $AdditionalBuildArguments = "--config", "$BuildType"
+} elseif($isLinux) {
+    $CMakeConfigureAruments = "-G", "Unix Makefiles"
+    $AdditionalBuildArguments = ""
+}
+
 New-Item $BuildDirectory -ItemType Directory -ErrorAction Continue
-cmake -S $SourceDirectory -B $BuildDirectory -D CMAKE_TOOLCHAIN_FILE=$ToolChainFile
-cmake --build $BuildDirectory
+cmake -S $SourceDirectory -B $BuildDirectory -D CMAKE_BUILD_TYPE=$BuildType -D CMAKE_TOOLCHAIN_FILE=$ToolChainFile $AdditionalConfigureArguments
+cmake --build $BuildDirectory $AdditionalBuildArguments
