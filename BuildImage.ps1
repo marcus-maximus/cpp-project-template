5a720253dc9b6d0df6dc3809450d6651ea050cbb
Marcus Schaber <marcus@hypercat.de>
2021-05-11 20:28:37 +0200

    Use cached when building docker image on azure devops.

--- BuildImage.ps1
+++ BuildImage.ps1
@@ -3,11 +3,14 @@ param(
     $ImageName = "cpp-project-build",
 
     [String]
-    $Dockerfile = $IsWindows ? "Dockerfile_windows" : "Dockerfile_linux"
+    $Dockerfile = $IsWindows ? "Dockerfile_windows" : "Dockerfile_linux",
+
+    [Parameter(Position=2, ValueFromRemainingArguments)]
+    $RemainingArguments
 )
 
 if($isLinux) {
     $AdditionalArguments = "--build-arg", "USER_ID=$(id -u)", "--build-arg", "GROUP_ID=$(id -g)"
 }
 
-docker build -t $ImageName -f $Dockerfile $AdditionalArguments .
+docker build -t $ImageName -f $Dockerfile $AdditionalArguments $RemainingArguments .
