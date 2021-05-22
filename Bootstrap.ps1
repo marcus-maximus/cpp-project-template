7de9005fe185d58944bb917a97b7ecaa7c3c9121
marcus-maximus <43315205+marcus-maximus@users.noreply.github.com>
2021-05-22 23:10:29 +0200

    Configurable vcpkg path (#4)
    * Make vcpkg path configurable for bootstrap script.
    
    Allows to use existing local installations.
    
    * Use explicit vcpkg path in azure pipelines.
    
    Co-authored-by: Marcus Schaber <marcus@hypercat.de>

--- Bootstrap.ps1
+++ Bootstrap.ps1
@@ -1,11 +1,23 @@
-git clone https://github.com/microsoft/vcpkg.git
+param (
+	[String]
+	$VcpkgPath = "./vcpkg"
+)
+
+$setupVcpkg = !(Test-Path -Path $VcpkgPath)
+$vcpkgURL = "https://github.com/microsoft/vcpkg.git"
 
 if($isWindows) {
-	./vcpkg/bootstrap-vcpkg.bat
-	./vcpkg/vcpkg install catch2:x64-windows
-	./vcpkg/vcpkg install rapidcheck:x64-windows
+	if ($setupVcpkg) {
+		git clone $vcpkgURL $VcpkgPath
+		& $VcpkgPath/bootstrap-vcpkg.bat
+	}
+	& $VcpkgPath/vcpkg install catch2:x64-windows
+	& $VcpkgPath/vcpkg install rapidcheck:x64-windows
 } elseif($isLinux) {
-	sh ./vcpkg/bootstrap-vcpkg.sh
-	./vcpkg/vcpkg install catch2:x64-linux
-	./vcpkg/vcpkg install rapidcheck:x64-linux
+	if ($setupVcpkg) {
+		git clone $vcpkgURL $VcpkgPath
+		sh $VcpkgPath/bootstrap-vcpkg.sh
+	}
+	& $VcpkgPath/vcpkg install catch2:x64-linux
+	& $VcpkgPath/vcpkg install rapidcheck:x64-linux
 }
