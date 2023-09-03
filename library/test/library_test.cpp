b2f59da7bde7ff64c06322c35787f4068132fe69
Marcus Schaber <marcus@hypercat.de>
2023-09-03 10:07:40 +0200

    Update tests for using catch with main

--- library/test/library_test.cpp
+++ library/test/library_test.cpp
@@ -1,6 +1,4 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include <catch.hpp>
-
+#include <catch2/catch_test_macros.hpp>
 #include <library.h>
 
 TEST_CASE("One plus one", "[add]")
