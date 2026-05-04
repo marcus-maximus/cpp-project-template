82fb0a71759a8ea27291641608ef28768d7d8ac5
Marcus Schaber <marcus@hypercat.de>
2026-05-04 21:35:17 +0200

    Update C++ standard to 23 and fix variable usage in main

--- executable/source/main.cpp
+++ executable/source/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     auto const a = 1;
     auto const b = 1;
-    auto result = add(1, 1);
+    auto result = add(a, b);
 
     std::cout << a << " + " << b << " equals " << result << std::endl;
 
